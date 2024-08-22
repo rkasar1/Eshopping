bfb03f062a2bd038ad0e70c48596579c66463336
unknown <rashmikasar150@gmail.com>
2024-08-22 15:45:10 +0530

    commit

---
+++ src/main/java/com/ecom/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.ecom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String index()
+	{
+		return "index";
+	}
+
+	@GetMapping("/register")
+	public String register()
+	{
+		return "signup";
+	}
+	
+	@GetMapping("/login")
+	public String login()
+	{
+		return "login";
+	}
+}
